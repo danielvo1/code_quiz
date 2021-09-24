5bf43242656f0c3657eb3d019270658455e82b57
Daniel Vo <daniel.vo@berkeley.edu>
2021-09-23 17:29:58 -0700

    added timer function with no styling

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// intializing global variables to be used in the functions
+var start_button = document.querySelector("#start");
+var question = document.querySelector("#question");
+var timer = document.querySelector("#timer");
+var total_time = 90;
+// the total amount of time for the test
+function countDown(){
+setInterval(function(){ 
+    const hour =  Math.floor(total_time/60);
+    console.log(hour);
+    const min = total_time % 60;
+    if (total_time > 0) {
+        timer.textContent = hour + ":" + min; 
+        total_time -= 1;
+}
+   
+}, 1000);
+};
+
+start_button.addEventListener('click', countDown);
+
+
