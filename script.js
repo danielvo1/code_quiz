70bc45371d67f7c7ba8aed68b44bd3cabce6e5a0
Daniel Vo <daniel.vo@berkeley.edu>
2021-09-27 19:24:03 -0700

    created a button dynamivally, and added question/answers

--- script.js
+++ script.js
@@ -3,22 +3,61 @@
 var start_button = document.querySelector("#start");
 var question = document.querySelector("#question");
 var timer = document.querySelector("#timer");
-var total_time = 5;
+var total_time = 90;
 var sec;
 var min;
 
+//array of questions and corresponding selections 
+var questions = [
+    "What does HTML stand for?", 
+    "What is CSS mainly used for?",
+    "What is my dog's name ?" 
+];
+
+console.log(questions);
+//answer key
+var answerKey = [
+    "Hypertext Markup Language" , 
+    "Styling", 
+    "Gigi"
+];
+
+var selections = [
+    {a: "Hypertext Markup Language" ,
+    b: "Hyperactive Markup Language" ,
+    c: "Hotdogs Mustard Lard" ,
+    d: "Hypertext Makeup Language" }, 
+
+   {a:"Creating Scripts" ,
+    b: "Styling",
+    c: "Main structure" ,
+    d: "Its not used" },
+
+   {a: "Spike" , 
+    b:  "Gigi" , 
+    c: "Kyrie" ,
+    d: "Billy" }
+]
+
+console.log(selections);
+
 // function starts the quiz
 function startQuiz() {
+    countDown();
+    createBtn();
 
 }
 
 
+function createBtn () {
+    var button = document.createElement('button');
+    button.innerHTML = "Next";
+    document.body.appendChild(button);
+}
 
+function quiz(){
 
-
-
-
-
+}
 
 
 
@@ -45,7 +84,6 @@ function countDown(){
 
         if (total_time > 0) {
             total_time -= 1;
-            console.log(total_time);
             timer.innerHTML = min + ':' + sec;
         
         } else if (total_time == 0) {
@@ -59,10 +97,5 @@ function countDown(){
 
 
 
-
-
-
-
-   
-start_button.addEventListener('click', countDown);
+start_button.addEventListener('click', startQuiz);
 
